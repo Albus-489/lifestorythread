97931cd609ddd8b95b6210fb3fa57c1d7c16a99f
Albus-489 <smartoctopusz@gmail.com>
2023-11-22 21:21:01 +0200

    component prop edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MonthComponent } from './components/month/month.component';
-import { DayComponent } from './components/day/day.component';
 import { DayPageComponent } from './components/day-page/day-page.component';
 
 const routes: Routes = [
