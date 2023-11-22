ed483b14f8bbd4a89985f259909ffca52f34780b
Albus-489 <bavrzar@gmail.com>
2023-11-22 20:34:26 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MonthComponent } from './components/month/month.component';
+import { DayComponent } from './components/day/day.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MonthComponent },
+  { path: 'day', component: DayComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
